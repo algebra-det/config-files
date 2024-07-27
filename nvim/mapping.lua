dbd479342f90941cfb29db5c7376f5664433b795
Algebra <60046240+algebra-det@users.noreply.github.com>
2024-07-28 02:10:01 +0530

    Update mapping.lua

--- nvim/mapping.lua
+++ nvim/mapping.lua
@@ -1,4 +1,11 @@
+---@type MappingsTable
 local M = {}
+
+M.disabled = {
+  i = {
+    ["<C-BS>"] = { "<C-w>", "Delete word back" },
+  },
+}
 
 M.general = {
   n = {
@@ -7,9 +14,24 @@ M.general = {
     ["<A-,>"] = { "<C-w>>", "decrease window size vertically" },
     ["<A-.>"] = { "<C-w><", "increase window size vertically" },
     ["<A-'>"] = { 'yiwo/* eslint-disable no-console */<CR>console.log("<Esc>pa: ", <Esc>pa);<Esc>', "Console log the word" },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    },
   },
-    i = {
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
+  i = {
     ["<C-BS>"] = { "<C-w>", "Delete word back" },
     ["<A-'>"] = { '<Esc>lyiwo/* eslint-disable no-console */<CR>console.log("<Esc>pa: ", <Esc>pa);<Esc>', "Console log the word" },
   },
 }
+
+-- more keybinds!
+
+return M
