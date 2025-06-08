ccecb20769fc665aabf7dccaccf9c19293d43737
Algebra <60046240+algebra-det@users.noreply.github.com>
2025-06-09 00:04:00 +0530

    Add files via upload

---
+++ nvim/nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set("n", "gl", "$", { silent = true })
+vim.keymap.set("i", "gh", "0", { silent = true })
+vim.keymap.set("n", "<leader>y", "Flash", { silent = true, desc = "Jump to Flash" })
+
+-- vim.keymap.set("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true })
+--
+--
